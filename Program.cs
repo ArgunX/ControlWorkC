4f963bd92df00739416e41d3b1c70cb312ece588
ArgunX <samooborona88@gmail.com>
2023-06-16 13:53:36 +0300

    Ввел функцию для форматирования массива по заданию

--- Program.cs
+++ Program.cs
@@ -1,3 +1,24 @@
 ﻿string[] InArray = new string[8] { "R2-D2", "Han Solo", "Anakin Skywalker", "Boba Fett", "Chewbacca", "Yoda", "C-3PO", "Jabba" };
 Console.WriteLine($" Вот исходный массив-> [{string.Join(", ", InArray)}]");
 int lenOut = new Random().Next(0, 4);
+
+string[] ArrayFormat(string[] array)
+
+{
+    string[] OutArray = new string[lenOut];
+
+    int randIndex = 0;
+    for (int i = 0; i < lenOut; i++)
+    {
+        randIndex = new Random().Next(0, InArray.Length);
+        while (ComporateRepeat(randIndex, OutArray,InArray))
+        {
+            randIndex = new Random().Next(0, InArray.Length);
+
+        }
+        OutArray[i] = InArray[randIndex];
+
+    }
+    return OutArray;
+
+}
