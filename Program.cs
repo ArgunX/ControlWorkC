8298ccc641c0ab80067c381844e76ba5cd0aca56
ArgunX <samooborona88@gmail.com>
2023-06-16 13:46:32 +0300

    Ввели исходные данные массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] InArray = new string[8] { "R2-D2", "Han Solo", "Anakin Skywalker", "Boba Fett", "Chewbacca", "Yoda", "C-3PO", "Jabba" };
+Console.WriteLine($" Вот исходный массив-> [{string.Join(", ", InArray)}]");
+int lenOut = new Random().Next(0, 4);
